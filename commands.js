85c057836df55a9440cad248a05941f41b4caa41
Daniel Fagnan <dnfagnan@gmail.com>
2013-03-04 22:35:29 -0700

    Working commands.
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

---
+++ commands.js
@@ -0,0 +1,3 @@
+exports.server      = require('./commands/server');
+exports.test        = require('./commands/test');
+exports.generate    = require('./commands/generate');
--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 module.exports = function() {
 
+    var program = require('commander');
+    var commands = require('./commands.js');
 
+    program
+        .version(require('./package.json').version)
+        .option('s,server,', 'Start the Tower server.')
+        .option('t,test', 'Run Tower tests.')
+        .option('g,generate', 'Run the generator.')
+        .parse(process.argv);
+
+    if (program.server || !process.argv[2]) {
+        commands.server();
+    } else if (program.test) {
+        commands.test();
+    } else if (program.generate) {
+        commands.generate();
+    } else {
+        // Throw a new error:
+        console.error('Command [' + process.argv[2] + "] was not found.");
+        process.exit();
+    }
 
 };
