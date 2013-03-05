1b7fadba9436c463f39e60a4fc303f175ab468ce
Daniel Fagnan <dnfagnan@gmail.com>
2013-03-04 22:39:02 -0700

    fixes
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ module.exports = function() {
         .option('s,server,', 'Start the Tower server.')
         .option('t,test', 'Run Tower tests.')
         .option('g,generate', 'Run the generator.')
+        .option('-v', 'Verbose Output.')
         .parse(process.argv);
 
+    var args = process.argv.splice(3);
+
     if (program.server || !process.argv[2]) {
-        commands.server();
+        commands.server(args);
     } else if (program.test) {
-        commands.test();
+        commands.test(args);
     } else if (program.generate) {
-        commands.generate();
+        commands.generate(args);
     } else {
         // Throw a new error:
         console.error('Command [' + process.argv[2] + "] was not found.");
